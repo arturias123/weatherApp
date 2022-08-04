a5ef7c5bbcca8b5700a9ebebd8403ce34a4d5fa3
arturias123 <52829156+arturias123@users.noreply.github.com>
2022-08-04 10:43:33 +0700

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'iUGew4AhBIjz6oKSWfMS7MyV7eFUXyil';
 
 // get weather info
 const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query, {mode: 'cors'});
@@ -14,7 +14,7 @@ const getWeather = async (id) => {
 // get city info
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query, {mode: 'cors'});
