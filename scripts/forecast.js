d602a398fe1a07477cb581c6c0a6d92bb136d6ea
arturias123 <52829156+arturias123@users.noreply.github.com>
2022-08-04 10:41:26 +0700

    Add files via upload

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,7 +5,7 @@ const getWeather = async (id) => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
-    const response = await fetch(base + query);
+    const response = await fetch(base + query, {mode: 'cors'});
 
     const data = await response.json();
     return data[0];
@@ -17,7 +17,7 @@ const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base + query);
+    const response = await fetch(base + query, {mode: 'cors'});
 
     const data = await response.json();
 
